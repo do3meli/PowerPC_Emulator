062574b4d6d6f9357e3e4acfe69f16096f10e07f
Dom <info@dominicschlegel.ch>
2012-10-15 20:05:32 +0200

    moved js to js folder

---
+++ js/ppc_emulator.js
@@ -0,0 +1,12 @@
+// this function converts a decimal input string into a binary two's complement
+function dec2bin(input) {
+	
+	alert(input.charAt(0));
+	if( input.charAt(0) == "-" ){
+	
+	}else{
+		var bin = input.toString(2);
+	}
+	
+	return bin;
+}
