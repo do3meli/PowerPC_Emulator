cd9400e801ba87297e98ce2e98ee5ba35f25204c
Dom <info@dominicschlegel.ch>
2012-10-15 21:19:05 +0200

    first implementation of binary funct

--- js/ppc_emulator.js
+++ js/ppc_emulator.js
@@ -1,12 +1,34 @@
-// this function converts a decimal input string into a binary two's complement
+// this function converts a decimal input string into a binary
 function dec2bin(input) {
+	return parseInt(input).toString(2);
+}
+
+// this function converts a decimal number into two's complement
+function twocomplement(x) {
 	
-	alert(input.charAt(0));
-	if( input.charAt(0) == "-" ){
-	
+	if(x < 0){
+		var string = x.toString();
+		var dec = dec2bin(string.substring(1));
+		
+		alert(dec);
+		
+		var invertstr = "";
+		for (i=1; i<=dec.length; i++){
+			
+		
+			if(parseInt(dec.substring(i)) == 1){
+				invertstr = invertstr + "0";	
+			}else{
+				invertstr = invertstr + "1";
+			}
+			
+			
+		}
+		alert(invertstr);
+		
+		return false;
 	}else{
-		var bin = input.toString(2);
+		return dec2bin(x);
 	}
-	
-	return bin;
+
 }
