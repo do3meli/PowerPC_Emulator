3ab6e8c5b1100c8df9237bada85cf6c24c4db650
Dom <info@dominicschlegel.ch>
2012-10-15 22:28:17 +0200

    2er komplement function

--- js/ppc_emulator.js
+++ js/ppc_emulator.js
@@ -9,24 +9,26 @@ function twocomplement(x) {
 	if(x < 0){
 		var string = x.toString();
 		var dec = dec2bin(string.substring(1));
-		
-		alert(dec);
-		
 		var invertstr = "";
-		for (i=1; i<=dec.length; i++){
+		alert(dec);
+		for (i=0; i<dec.length; i++){
 			
-		
-			if(parseInt(dec.substring(i)) == 1){
+			if(parseInt(dec.substring(i,i+1)) == 1){
 				invertstr = invertstr + "0";	
 			}else{
 				invertstr = invertstr + "1";
 			}
-			
-			
 		}
 		alert(invertstr);
-		
-		return false;
+		// if last sign == 0 then just replace it by a 1
+		if(invertstr.substr(invertstr.length - 1) == 0){
+			return invertstr.substr(0,invertstr.length - 1) + "1";
+		}else{
+			// add in decimal system 1 and convert back to binary
+			res = parseInt(invertstr,2) + 1
+			return res.toString(2);		
+		}
+	
 	}else{
 		return dec2bin(x);
 	}
