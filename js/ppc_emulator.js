43acaac9e6d08fb9bd2685a3e284151d960b9102
Dom <info@dominicschlegel.ch>
2012-10-16 11:49:45 +0200

    small changeds and comments

--- js/ppc_emulator.js
+++ js/ppc_emulator.js
@@ -229,7 +229,7 @@ function getAkku(){
 }
 
 
-//input value
+// generates a twocomplement of the input value and stores it in corresponding fields in GUI
 function setInputValueinGui(value,field,number){
 	var twoComplement = twocomplement(value,16);
 	$("#" + field + "Bin").val(twoComplement.substring(0,8));
@@ -238,6 +238,7 @@ function setInputValueinGui(value,field,number){
 	$("#" + secondField + "Bin").val(twoComplement.substring(8));
 }
 
+// stores the mnemonic in correct storage array field and calles GUI update function
 function setInputValueBin(value,field){
 	var numberFind = new RegExp('[0-9]+', 'g');
 	var number = field.match(numberFind);
@@ -245,12 +246,13 @@ function setInputValueBin(value,field){
 	setInputValueinGui(value,field,number);
 }
 
+// this function is getting called when an input value has been changed in GUI
 function updateInputValue(event){
 	var elementId = event.target.id;
 	setInputValueBin(event.target.value, elementId);
 }
 
-//programm code
+// This functions generates the machinecode and stores it into the corresponding fields in GUI
 function setProgrammCodeinGui(value,field,number){
 	var binValue=mnemonic2machinecode(value)
 	$("#" + field + "Bin").val(binValue.substring(0,8));
@@ -259,6 +261,7 @@ function setProgrammCodeinGui(value,field,number){
 	$("#" + secondField + "Bin").val(binValue.substring(8));
 }
 
+// stores the mnemonics and calls the method which updates the GUI with binary code
 function setProgrammCodeBin(value,field){
 	var numberFind = new RegExp('[0-9]+', 'g');
 	var number = field.match(numberFind);
@@ -266,6 +269,8 @@ function setProgrammCodeBin(value,field){
 	setProgrammCodeinGui(value,field,number);
 }
 
+// this function gets called when the field in GUI is getting changed
+// it determines the correct elementID and calles the setProgrammCodeBin method
 function updateProgrammCode(event){
 	var elementId = event.target.id;
 	setProgrammCodeBin(event.target.value, elementId);
@@ -333,7 +338,7 @@ function defineAction(x,executeCommand){
 	var keyword = x.match(keywordFind);
 	var output = "";
 	var outputBin = "";
-	//alert(keyword);
+
 	if (keyword == "CLR"){
 		var registerNumber=findRegister(x);
 		output = "CLR register: " + registerNumber;
@@ -341,7 +346,8 @@ function defineAction(x,executeCommand){
 		if(executeCommand){
 			//clear register
 			setReg(0,registerNumber);
-		}	
+		}
+	
 	} else 	if (keyword == "ADD"){
 		var registerNumber=findRegister(x);
 		output = "ADD to register: " + registerNumber;
@@ -350,97 +356,121 @@ function defineAction(x,executeCommand){
 			//add register to akku
 			setAkku(getAkku() + getReg(registerNumber));
 		}				
+	
 	} else 	if (keyword == "ADDD"){
 		var numberFind = new RegExp('[-]?[1-9]+', 'g');
 		var number = x.match(numberFind);
 		output = "ADDD " + number;
 		outputBin = "1" + twocomplement(number,15);		
+	
 	} else 	if (keyword == "INC"){
 		output = "INC";
 		outputBin = "0000000100000000";	
+	
 	} else 	if (keyword == "DEC"){
 		output = "DEC";
 		outputBin = "0000010000000000";		
+	
 	} else 	if (keyword == "LWDD"){
 		var registerNumber=findRegister(x);
 		var address=findAddress(x)
 		output = "LWDD from register: " + registerNumber + " to address " + address;
 		outputBin = "0100" + dec2bin(registerNumber) + dec2bin(address);		
+	
 	} else 	if (keyword == "SWDD"){
 		var registerNumber=findRegister(x);
 		var address=findAddress(x);
 		output = "SWDD to register: " + registerNumber + " from address " + address;
 		outputBin = "0110" + dec2bin(registerNumber) + dec2bin(address);				
+	
 	} else 	if (keyword == "SRA"){
 		output = "SRA";	
 		outputBin = "0000010100000000";	
+	
 	} else 	if (keyword == "SLA"){
 		output = "SLA";
 		outputBin = "0000100000000000";		
+	
 	} else 	if (keyword == "SRL"){
 		output = "SRL";
 		outputBin = "0000100100000000";		
+	
 	} else 	if (keyword == "SLL"){
 		output = "SLL";
 		outputBin = "0000110000000000";			
+	
 	} else 	if (keyword == "AND"){
 		var registerNumber=findRegister(x);
 		output = "AND register: " + registerNumber;
 		outputBin = "0000" + dec2bin(registerNumber) + "1000000000";		
+	
 	} else 	if (keyword == "OR"){
 		var registerNumber=findRegister(x);
 		output = "OR register: " + registerNumber;
 		outputBin = "0000" + dec2bin(registerNumber) + "1100000000";	
+	
 	} else 	if (keyword == "NOT"){
 		output = "NOT";
 		outputBin = "0000000010000000";		
+	
 	} else 	if (keyword == "BZ"){
 		var registerNumber=findRegister(x);
 		output = "BZ register: " + registerNumber;
 		outputBin = "0001" + dec2bin(registerNumber) + "1000000000";	
+	
 	} else 	if (keyword == "BNZ"){
 		var registerNumber=findRegister(x);
 		output = "BNZ register: " + registerNumber;
 		outputBin = "0001" + dec2bin(registerNumber) + "0100000000";
+	
 	} else 	if (keyword == "BC"){
 		var registerNumber=findRegister(x);
 		output = "BC register: " + registerNumber;
 		outputBin = "0001" + dec2bin(registerNumber) + "1100000000";	
+	
 	} else 	if (keyword == "B"){
 		var registerNumber=findRegister(x);
 		output = "B register: " + registerNumber;
 		outputBin = "0001" + dec2bin(registerNumber) + "0000000000";	
+	
 	} else 	if (keyword == "BZD"){
 		var address=findAddress(x);
 		output = "BZD to address " + address;
 		outputBin = "001100" + dec2bin(address);	
+	
 	} else 	if (keyword == "BNZD"){
 		var address=findAddress(x);
 		output = "BNZD to address " + address;
 		outputBin = "001010" + dec2bin(address);		
+	
 	} else 	if (keyword == "BCD"){
 		var address=findAddress(x);
 		output = "BCD to address " + address;
 		outputBin = "001110" + dec2bin(address);		
+	
 	} else 	if (keyword == "BD"){
 		var address=findAddress(x);
 		output = "BD to address " + address;
 		outputBin = "001000" + dec2bin(address);		
+	
 	} else 	if (keyword == "STOP"){
 		output = "STOP";
 		outputBin = "0000000000000000";														
+	
 	} else {
+		// if command is not known alert that
 		output = "unknown command";
+		alert(output);
 	}
-	if (outputBin.length < 16){
+	
+	// check lenght and alert if not correct
+	if (outputBin.length < 16 && output != "unknown command"){
 		alert("Not enought bits");
-	} else if (outputBin.length > 16){
+	} else if (outputBin.length > 16 && output != "unknown command"){
 		alert("Too many bits");
 	}
-	//var outputLine1=outputBin.substring(0,8);
-	//var outputLine2=outputBin.substring(8);
-	//alert('Output:' + output + '\n' + 'Bin output:' + outputBin);
-	//alert('Bin:' + outputBin + '\n' +'Line1:' + outputLine1 + '\n' + 'Line2:' + outputLine2);
+	
+	// return value
 	return outputBin;
 } 
 
